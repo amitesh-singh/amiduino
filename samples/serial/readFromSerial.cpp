d4cc1845a4c809f621c851a79cde6bf0f008871b
Amitesh Singh <singh.amitesh@gmail.com>
2016-02-03 21:29:37 +0530

    add more comments

--- samples/serial/readFromSerial.cpp
+++ samples/serial/readFromSerial.cpp
@@ -4,6 +4,7 @@ char readByte;
 void setup()
 {
   pinMode(13, OUTPUT);
+  // initialize serial connection with 9600 baud rate
   Serial.begin(9600);
   Serial.println("Starting to read from Serial input -- 1 means LED on and 0 mean LED off");
 }
@@ -13,6 +14,7 @@ void loop()
   if (Serial.available())
   {
     //convert char into int (only works with 0 - 9 input)
+    // read the input from serial port
     int incomingState = (Serial.read() - '0');
     if (incomingState != state)
     {
