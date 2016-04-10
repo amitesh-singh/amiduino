457b3d6ff54d1dd50597c52e23276b42adcfa2bb
Amitesh Singh <singh.amitesh@gmail.com>
2016-04-10 14:31:21 +0530

    copy my version blink

--- avr/ami-arduino-cmake/sketches/blink/blink.cpp
+++ avr/ami-arduino-cmake/sketches/blink/blink.cpp
@@ -1,24 +1,3 @@
-/**
- * ...
- *
- * @author Matthias L. Jugel
- *
- * Copyright 2015 ubirch GmbH (http://www.ubirch.com)
- *
- * == LICENSE ==
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 #include <Arduino.h>
 
 #define LED 13
@@ -27,11 +6,6 @@ void setup() {
     // set the LED pin to be an output pin
     pinMode(LED, OUTPUT);
 }
-
-// this tells the IDE and the compiler, that it's okay to have an endless loop
-// (we never return from this function)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wmissing-noreturn"
 
 void loop() {
 
@@ -44,5 +18,3 @@ void loop() {
         delay(200);
     }
 }
-
-#pragma clang diagnostic pop
