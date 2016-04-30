dcac189fa7fb1a291d51720370b3b069c7e7eb34
Amitesh Singh <singh.amitesh@gmail.com>
2016-04-30 14:17:50 +0530

    add blink example for atmega16a

---
+++ avr/ami-atmega16-cmake/sketches/blink/blink.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+#define LED 13
+
+void setup() {
+    // set the LED pin to be an output pin
+    pinMode(LED, OUTPUT);
+}
+
+void loop() {
+
+    while (true) {
+        // write a 1 (digital signal high)
+        digitalWrite(LED, HIGH);
+        delay(100);
+        // write a 0 (digital signal low)
+        digitalWrite(LED, LOW);
+        delay(200);
+    }
+}
