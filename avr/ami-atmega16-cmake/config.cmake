56a4a54ec6df9d1bdc64130c66cf87b180ba258b
Amitesh Singh <singh.amitesh@gmail.com>
2016-04-30 15:47:52 +0530

    update gitignore

--- avr/ami-atmega16-cmake/config.cmake
+++ avr/ami-atmega16-cmake/config.cmake
@@ -2,7 +2,7 @@
 #set(MCU atmega328p)
 set(MCU atmega16a)
 set(F_CPU 1000000)
-set(BAUD 115200)
+set(BAUD 95600)
 
 # set the programmer and the device to use (default is to use simavr)
 set(PROGRAMMER usbasp)
@@ -18,4 +18,5 @@ set(EXTRA_LIBS "-Wl,--trace")
 
 # if you want to compile Arduino sketches, set this to the Arduino SDK dir (1.6.5)
 #set(ARDUINO_SDK_PATH "/Applications/Arduino.app/Contents/Java" CACHE STRING "Arduino SDK Path")
+# note: software serial libraries won't compile for atmega16a hence remove it from libraries/
 set(ARDUINO_SDK_PATH "/home/ami/Downloads/arduino-1.6.8-atmega16a" CACHE STRING "Arduino SDK Path")
