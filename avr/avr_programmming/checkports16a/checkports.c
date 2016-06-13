1bc864b273b4cf1d8ee4f04e00e1cb7f03fbdec6
Amitesh Singh <singh.amitesh@gmail.com>
2016-06-13 15:59:57 +0530

    AVR: Add atmega16a blink code for checking all digital ports

---
+++ avr/avr_programmming/checkports16a/checkports.c
@@ -0,0 +1,35 @@
+//#define __AVR_ATmega16A__ ---> we are passing this macro as compile option in avr-gcc -mmcu=atmega16a
+#include <avr/io.h>
+#include <util/delay.h>
+
+//This program checks all digital ports
+// and do a classic LED blink on all ports
+int main()
+{
+   DDRB = 0xff;
+   DDRC = 0xff;
+   DDRD = 0xff;
+
+   while (1)
+     {
+        PORTB = 0xff;
+        PORTC = 0xff;
+        PORTD = 0xff;
+        _delay_ms(100);
+        PORTB = 0;
+        PORTC = 0;
+        PORTD = 0;
+        _delay_ms(100);
+
+        PORTB = 0xff;
+        PORTC = 0xff;
+        PORTD = 0xff;
+        _delay_ms(60);
+        PORTB = 0;
+        PORTC = 0;
+        PORTD = 0;
+        _delay_ms(60);
+     }
+
+   return 0;
+}
