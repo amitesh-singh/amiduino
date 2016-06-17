698b56f4878839ca876df4dd7b707884ba162b63
Amitesh Singh <singh.amitesh@gmail.com>
2016-06-18 02:13:18 +0530

    LCD: goto example

---
+++ avr/avr_programmming/lcd/gotoXY/game.c
@@ -0,0 +1,140 @@
+#define F_CPU 8000000UL
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h> //for itoa
+
+#define MrLCDsCrib PORTB
+#define DataDir_MrLCDsCrib DDRB
+#define MrLCDsControl PORTD
+#define DataDir_MrLCDsControl DDRD
+#define LightSwitch 3 // E
+#define ReadWrite 4  // RW
+#define BiPolarMood 2 //RS
+
+void Check_IF_MrLCD_isBusy(void);
+void Peek_A_Boo(void);
+void Send_A_Command(unsigned char command);
+void Send_A_Character(unsigned char character);
+void Send_A_String(char *string);
+ //                                    Row: 1  2    3   4
+char colPosCommandAddress[2] = {0, 64}; //  0, 64, 20, 84
+void gotoxyPos(uint8_t x, uint8_t y)
+{
+	if (x < 0)
+		x = 0;
+	else if (x > 16)
+		x = 16;
+	if (y < 0) y = 0;
+	else if (y > 1) y = 1;
+
+	Send_A_Command(0x80 + colPosCommandAddress[y] + x);
+}
+
+void Send_A_String(char *str)
+{
+	char *tmp = str;
+	while (*tmp != '\0')
+	{
+		Send_A_Character(*tmp++);
+	}
+}
+
+int main(void)
+{
+	DataDir_MrLCDsControl |= 1<<LightSwitch | 1<<ReadWrite | 1<<BiPolarMood;
+	_delay_ms(15);
+
+	Send_A_Command(0x01); //Clear Screen 0x01 = 00000001
+	_delay_ms(2);
+	Send_A_Command(0x38);
+	_delay_us(50);
+	Send_A_Command(0b00001110);
+	_delay_us(50);
+
+	Send_A_String("X-Y game ;)");
+
+	_delay_ms(10000); // show it for 10 seconds
+
+	Send_A_Command(0x01);
+	_delay_ms(2);
+
+	char pos[3];
+	while(1)
+	{
+		for (uint8_t i = 0; i < 16; ++i)
+		{
+			pos[0] = 0;
+			pos[1] = 0;
+			pos[2] = 0;
+			//Send_A_Command(0x80 + i);
+			gotoxyPos(i, 0);
+			_delay_ms(1);
+			Send_A_String("X");
+
+			_delay_ms(500);
+
+			gotoxyPos(14, 1);
+			_delay_ms(1);
+			Send_A_String("  ");
+
+			itoa(i, pos, 10);
+			//Send_A_Command(0x80 + 75);
+			gotoxyPos(14, 1);
+			_delay_ms(1);
+			Send_A_String(pos);
+			_delay_ms(1);
+
+			//Send_A_Command(0x80 + i);
+			gotoxyPos(i, 0);
+
+			_delay_ms(1);
+			Send_A_String(" "); //erase X from its old position
+			_delay_ms(1);
+		}
+
+	}
+}
+
+void Check_IF_MrLCD_isBusy()
+{
+	DataDir_MrLCDsCrib = 0;
+	MrLCDsControl |= 1<<ReadWrite;
+	MrLCDsControl &= ~1<<BiPolarMood;
+
+	while (MrLCDsCrib >= 0x80)
+	{
+		Peek_A_Boo();
+	}
+	DataDir_MrLCDsCrib = 0xFF; //0xFF means 0b11111111
+	_delay_ms(1); //this wait was necessary to make it work - Ami
+}
+
+void Peek_A_Boo()
+{
+	MrLCDsControl |= 1<<LightSwitch;
+	asm volatile ("nop");
+	asm volatile ("nop");
+	asm volatile ("nop");
+	asm volatile ("nop");
+	MrLCDsControl &= ~1<<LightSwitch;
+}
+
+void Send_A_Command(unsigned char command)
+{
+	Check_IF_MrLCD_isBusy();
+	MrLCDsCrib = command;
+	MrLCDsControl &= ~ ((1<<ReadWrite)|(1<<BiPolarMood));
+	Peek_A_Boo();
+	MrLCDsCrib = 0;
+}
+
+void Send_A_Character(unsigned char character)
+{
+	Check_IF_MrLCD_isBusy();
+	MrLCDsCrib = character;
+	MrLCDsControl &= ~ (1<<ReadWrite);
+	MrLCDsControl |= 1<<BiPolarMood;
+	Peek_A_Boo();
+	MrLCDsCrib = 0;
+}
