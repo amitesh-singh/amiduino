a69c42fad9911774f5714bf32abf435985a8c747
Amitesh Singh <singh.amitesh@gmail.com>
2016-06-26 15:00:10 +0530

    custom class test code for HOST.

--- avr/attiny85/custom-class/cdc/main.c
+++ avr/attiny85/custom-class/cdc/main.c
@@ -1,11 +1,9 @@
 /*
-This example should run on most AVRs with only little changes. No special
-hardware resources except INT0 are used. You may have to change usbconfig.h for
-different I/O pins for USB. Please note that USB D+ must be the INT0 pin, or
-at least be connected to INT0 as well.
 
-We use VID/PID 0x046D/0xC00E which is taken from a Logitech mouse. Don't
-publish any hardware using these IDs! This is for demonstration only!
+   connect D+ (without any caps, 100 pf(104) was creating issues) to INT0 (PB2)
+   connect D- (without any caps, 100 pf (104) was creating issues) to PB0
+   connect a LED with resistor on PB1 to Ground
+
 */
 
 #include <avr/io.h>
@@ -17,13 +15,10 @@ publish any hardware using these IDs! This is for demonstration only!
 #include "usbdrv.h"
 #include "oddebug.h"        /* This is also an example for using debug macros */
 
-/* ------------------------------------------------------------------------- */
-/* ----------------------------- USB interface ----------------------------- */
-/* ------------------------------------------------------------------------- */
-
 #define USB_LED_ON 1
 #define USB_LED_OFF 0
 
+//This is where custom message is handled from HOST
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
    usbRequest_t *rq = (void *)data; // cast data to correct type
@@ -31,10 +26,10 @@ usbMsgLen_t usbFunctionSetup(uchar data[8])
    switch(rq->bRequest)
      { // custom command is in the bRequest field
       case USB_LED_ON:
-         PORTB |= (1 << PB0); // turn LED on
+         PORTB |= (1 << PB1); // turn LED on
          return 0;
       case USB_LED_OFF:
-         PORTB &= ~(1 << PB0); // turn LED off
+         PORTB &= ~(1 << PB1); // turn LED off
          return 0;
      }
 
@@ -44,33 +39,32 @@ usbMsgLen_t usbFunctionSetup(uchar data[8])
 
 int __attribute__((noreturn)) main(void)
 {
-uchar   i;
+   uchar   i;
+   DDRB |= (1 << PB1); //define DDRB before doing usb stuffs
 
-    wdt_enable(WDTO_1S);
-    /* Even if you don't use the watchdog, turn it off here. On newer devices,
-     * the status of the watchdog (on/off, period) is PRESERVED OVER RESET!
-     */
-    /* RESET status: all port bits are inputs without pull-up.
-     * That's the way we need D+ and D-. Therefore we don't need any
-     * additional hardware initialization.
-     */
-    odDebugInit();
-    DBG1(0x00, 0, 0);       /* debug output: main starts */
-    usbInit();
-    usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
-    i = 0;
-    while(--i){             /* fake USB disconnect for > 250 ms */
+   wdt_enable(WDTO_1S);
+   /* Even if you don't use the watchdog, turn it off here. On newer devices,
+    * the status of the watchdog (on/off, period) is PRESERVED OVER RESET!
+    */
+   /* RESET status: all port bits are inputs without pull-up.
+    * That's the way we need D+ and D-. Therefore we don't need any
+    * additional hardware initialization.
+    */
+   odDebugInit();
+   DBG1(0x00, 0, 0);       /* debug output: main starts */
+   usbInit();
+   usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
+   i = 0;
+   while(--i){             /* fake USB disconnect for > 250 ms */
         wdt_reset();
         _delay_ms(1);
-    }
-    usbDeviceConnect();
-    sei();
-    DBG1(0x01, 0, 0);       /* debug output: main loop starts */
-    for(;;){                /* main event loop */
+   }
+   usbDeviceConnect();
+   sei();
+   DBG1(0x01, 0, 0);       /* debug output: main loop starts */
+   for(;;){                /* main event loop */
         DBG1(0x02, 0, 0);   /* debug output: main loop iterates */
         wdt_reset();
         usbPoll();
-    }
+   }
 }
-
-/* ------------------------------------------------------------------------- */
