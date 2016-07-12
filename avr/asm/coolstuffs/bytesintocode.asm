5fac211e09a8a16eb2e714e2fb6f993a73de2935
Amitesh Singh <singh.amitesh@gmail.com>
2016-07-12 20:44:49 +0530

    Add avra assembler binary for windows

--- avr/asm/coolstuffs/bytesintocode.asm
+++ avr/asm/coolstuffs/bytesintocode.asm
@@ -3,3 +3,8 @@
 .dw 0xe001 ; move r16, 0x01
 .dw 0x0f00 ; add r16, r16
 .dw 0xcfff ; rjmp 0x0002
+
+
+.db 0x03, 0x95 ; below two statements are similar to what this statement does
+; .dw 0x9503     
+; inc r 16
