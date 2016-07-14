2b157e4f446c6255fbdf6a36dd30265d50700fb8
Amitesh Singh <singh.amitesh@gmail.com>
2016-07-15 00:42:48 +0530

    fix the blink code to actually work for 1s delay.

--- avr/asm/blink/blink_1s.asm
+++ avr/asm/blink/blink_1s.asm
@@ -11,7 +11,6 @@
 .set RAMEND = $45f
 
 .cseg
-
 .org 0
 
 ; initialize STACK
@@ -20,31 +19,29 @@ _start:
   out spl, r16
   ldi r16, HIGH(RAMEND)
   out sph,  r16
-  
-  ldi r16, 0b00000001
-  out DDRB, r16
+
+  sbi DDRB, 0    ; can use ldi r16, 0b00000001 ; out DDRB, r16 ; since we are 
+                 ; only setting 1 byte, just use sbi instead.
   clr r17
 
 loop:
-  out PORTB, r16
+  sbi PORTB, 0
   rcall _delay_1s
   out PORTB, r17
   rcall _delay_1s
   rjmp loop
-
-ret
+;ret ; remove this also since we are not gonna reach here. its waste of 2 bytes for us.
 
 ; below function gives 8024195 cycles ~~ 1.003024 seconds ; not very accurate though.
 _delay_1s:
    ldi r18, 64 ; 1 clk cycle
-   ldi r19, 125; ; 1 clk cycle
-   ldi r20, 250; 1 clk
   ; 8 Mhz = 8 x 10^6 cycles
-  ; can't use r16 or r17 as it is getting used so lets use r18, r19 and r10 if required
   delay_1:
+   ldi r19, 125; ; 1 clk cycle
   delay_2:
+  ldi r20, 250; 1 clk
   delay_3:
-	nop         ; 1 clk
+  nop         ; 1 clk
 	dec r20 ; 1 clk
 	brne delay_3; 2 clk if jmp  and 1 clk if continue
    
