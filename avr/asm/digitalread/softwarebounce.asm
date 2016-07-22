dfd0ee99fea2398bf017f98b17c47912c1aef30e
Amitesh Singh <singh.amitesh@gmail.com>
2016-07-22 22:05:09 +0530

    asm: digital read example added

--- avr/asm/digitalread/softwarebounce.asm
+++ avr/asm/digitalread/softwarebounce.asm
@@ -1,3 +1,7 @@
+; This examples shows how to handle bouncing when button is pressed
+;
+;
+
 .include "../defs/m16def.inc"
 
 .def temp = r16
@@ -8,11 +12,32 @@
 .org 0
 
 _start:
+   ; stack init
+   ldi temp, LOW(RAMEND)
+   out SPL, temp
+   ldi temp, HIGH(RAMEND)
+   out SPH, temp
+
    rjmp main
+
+_delay_1ms:
+   ldi r21, 80 ; 1
+   upper_loop:
+      ldi r20, 250 ; 1
+      int_loop:
+         dec r20 ; 1
+         cpi r20, 0 ; 1
+         brne int_loop ; jmp 2 and passthrough 1
+
+      dec r21 ; 1
+      cpi r21, 0 ; 1
+      brne upper_loop ; jmp 2 and passthrough 1
+   ret
 
 ledson:
    com switch ; all bits are flipped
-   out PORTB, switch
+   eor leds, switch
+   out PORTB, leds
    rjmp comebackagain + 2; ; or at release?
 
 main:
@@ -24,7 +49,7 @@ main:
     com temp
     out DDRB, temp
 
-    out PORTD, temp ; enable internal pullups
+    out PORTD, temp
 
     loop:
     ; check for button to be pressed
@@ -32,6 +57,13 @@ main:
          in switch, PIND
          cpi switch, 0xff ; switch == 0xff;
          breq press
+
+      rcall _delay_1ms
+      ; do check for button pressed or not
+      press_2:
+         in switch, PIND
+         cpi switch, 0xff
+         breq press_2
 
       ;do something when either of the switch is pressed
       comebackagain:
