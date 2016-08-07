4c46555abccc24440fab9a10767fb5fa04a7b567
Amitesh Singh <singh.amitesh@gmail.com>
2016-08-07 14:37:43 +0530

    overclocking: running blink example at 30 MHz

---
+++ avr/attiny85/overclocking/blink/blink.c
@@ -0,0 +1,24 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+   //init
+   //DDRB |= 0b0000001;
+   DDRB |= (1 << PB0);
+
+   //event loop
+
+   while (1)
+     {
+        //PORTB = 0b00000001;          /* Turn on first LED bit/pin in PORTB */
+        PORTB |= (1 << PB0);          /* Turn on first LED bit/pin in PORTB */
+        _delay_ms(100);                                           /* wait */
+
+        //PORTB = 0b00000000;          /* Turn off all B pins, including LED */
+        PORTB = 0x00;          /* Turn off all B pins, including LED */
+        _delay_ms(1000);
+     }
+
+   return 0;
+}
