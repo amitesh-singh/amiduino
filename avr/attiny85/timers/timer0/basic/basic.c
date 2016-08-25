c71764b63d25e2522603857ce3e48f69fc79c8fb
Amitesh Singh <singh.amitesh@gmail.com>
2016-08-25 22:56:54 +0530

    remain online always by this nice random mouse ;)
    timeout is set to 1 min. should take less power

--- avr/attiny85/timers/timer0/basic/basic.c
+++ avr/attiny85/timers/timer0/basic/basic.c
@@ -7,11 +7,11 @@ static void
 _init_timer0()
 {
    //TCCR0A = 0; Setting WGM00 & WGM01 = 0 - normal mode
-   TCCR0B = (1 << CS00) | ( 1 << CS02); //no prescaling
+   TCCR0B = (1 << CS00) | ( 1 << CS02); // prescaling = 1024
    TCNT0 = 0; // i need 15 ms delay? 
    //  
    //  = Required Delay / clock time period - 1
-   // = 15 * 10^3 / (1 / 16.5/1024 * 10^6) - 1 = 241
+   // = 15 * 10^3 / (1 / 8/1024 * 10^6) - 1 = 241
    //
 }
 
@@ -23,7 +23,7 @@ void main()
 
    while (1)
      {
-        if (TCNT0 >= 241)
+        if (TCNT0 >= 249)
           {
              PORTB ^= (1 << PB0); // toggle LED on every 16 ms
              TCNT0 = 0;
--- avr/attiny85/vusb/hid-mouse-random/main.c
+++ avr/attiny85/vusb/hid-mouse-random/main.c
@@ -1,4 +1,3 @@
-
 #include <avr/io.h>
 #include <avr/wdt.h>
 #include <avr/interrupt.h>  /* for sei() */
@@ -6,41 +5,7 @@
 
 #include <avr/pgmspace.h>   /* required by usbdrv.h */
 #include "usbdrv.h"
-#include "oddebug.h"        /* This is also an example for using debug macros */
 
-/* ------------------------------------------------------------------------- */
-/* ----------------------------- USB interface ----------------------------- */
-/* ------------------------------------------------------------------------- */
-
-/*
-   /------------------------------------------
-// Standard Device Descriptor Type Definition
-//------------------------------------------
-typedef struct
-{
-BYTE bLength;                // Size of this Descriptor in Bytes
-BYTE bDescriptorType;        // Descriptor Type (=1)
-WORD bcdUSB;                 // USB Spec Release Number in BCD
-BYTE bDeviceClass;           // Device Class Code
-BYTE bDeviceSubClass;        // Device Subclass Code
-BYTE bDeviceProtocol;        // Device Protocol Code
-BYTE bMaxPacketSize0;        // Maximum Packet Size for EP0 
-WORD idVendor;               // Vendor ID 
-WORD idProduct;              // Product ID
-WORD bcdDevice;              // Device Release Number in BCD
-BYTE iManufacturer;          // Index of String Desc for Manufacturer
-BYTE iProduct;               // Index of String Desc for Product
-BYTE iSerialNumber;          // Index of String Desc for SerNo
-BYTE bNumConfigurations;     // Number of possible Configurations
-} device_descriptor;            // End of Device Descriptor Type
-This  declaration  exactly  conforms  to  the  USB  specification’s  requirements  for  the  size  and  content  ordorder  of  the
-device descriptor. Some contents are stored in single bytes while others require two bytes
-
-A USB HID report descriptor is one of the descriptors that a USB host can request from a USB device. HID devices send data to the host using reports, and the descriptor tells the host how to interpret the data.
-
-
-http://eleccelerator.com/tutorial-about-usb-hid-report-descriptors/
- */
 const PROGMEM char usbHidReportDescriptor[52] = { /* USB report descriptor, size must match usbconfig.h */
      0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
      0x09, 0x02,                    // USAGE (Mouse)
@@ -70,13 +35,7 @@ const PROGMEM char usbHidReportDescriptor[52] = { /* USB report descriptor, size
      0xC0,                          //   END_COLLECTION
      0xC0,                          // END COLLECTION
 };
-/* This is the same report descriptor as seen in a Logitech mouse. The data
- * described by this descriptor consists of 4 bytes:
- *      .  .  .  .  . B2 B1 B0 .... one byte with mouse button states
- *     X7 X6 X5 X4 X3 X2 X1 X0 .... 8 bit signed relative coordinate x
- *     Y7 Y6 Y5 Y4 Y3 Y2 Y1 Y0 .... 8 bit signed relative coordinate y
- *     W7 W6 W5 W4 W3 W2 W1 W0 .... 8 bit signed relative coordinate wheel
- */
+
 typedef struct{
      uchar   buttonMask;
      char    dx;
@@ -87,8 +46,14 @@ typedef struct{
 static report_t reportBuffer;
 static uchar    idleRate;   /* repeat rate for keyboards, never used for mice */
 
-
-/* ------------------------------------------------------------------------- */
+static void
+_timer1_init()
+{
+   // timeout = 15ms
+   TCCR0B = (1 << CS00) | ( 1 << CS02);
+   // presacalar = 1024
+   TCNT0 = 0;
+}
 
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
@@ -97,59 +62,62 @@ usbMsgLen_t usbFunctionSetup(uchar data[8])
    /* The following requests are never used. But since they are required by
     * the specification, we implement them in this example.
     */
-   if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
-        DBG1(0x50, &rq->bRequest, 1);   /* debug output: print our request */
-        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
+   if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS)
+     {    /* class request type */
+        if(rq->bRequest == USBRQ_HID_GET_REPORT)
+          {  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
              /* we only have one report type, so don't look at wValue */
              usbMsgPtr = (void *)&reportBuffer;
              return sizeof(reportBuffer);
-        }else if(rq->bRequest == USBRQ_HID_GET_IDLE){
+          }
+        else if(rq->bRequest == USBRQ_HID_GET_IDLE)
+          {
              usbMsgPtr = &idleRate;
              return 1;
-        }else if(rq->bRequest == USBRQ_HID_SET_IDLE){
+          }
+        else if(rq->bRequest == USBRQ_HID_SET_IDLE)
+          {
              idleRate = rq->wValue.bytes[1];
-        }
-   }else{
+          }
+     }
+   else
+     {
         /* no vendor specific requests implemented */
-   }
+     }
    return 0;   /* default for not implemented requests: return no data back to host */
 }
 
-/* ------------------------------------------------------------------------- */
 
 int __attribute__((noreturn)) main(void)
 {
+   int timeout = 0;
    uchar   i;
    int rand = 3765;
 
    wdt_enable(WDTO_1S);
-   /* Even if you don't use the watchdog, turn it off here. On newer devices,
-    * the status of the watchdog (on/off, period) is PRESERVED OVER RESET!
-    */
-   /* RESET status: all port bits are inputs without pull-up.
-    * That's the way we need D+ and D-. Therefore we don't need any
-    * additional hardware initialization.
-    */
-   odDebugInit();
-   DBG1(0x00, 0, 0);       /* debug output: main starts */
    usbInit();
    usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
    i = 0;
    while(--i)
      {             /* fake USB disconnect for > 250 ms */
         wdt_reset();
-        _delay_ms(1);
+        _delay_ms(2);
      }
    usbDeviceConnect();
+   _timer1_init();
    sei();
-   DBG1(0x01, 0, 0);       /* debug output: main loop starts */
    for(;;)
      {                /* main event loop */
-        DBG1(0x02, 0, 0);   /* debug output: main loop iterates */
         wdt_reset();
         usbPoll();
-        _delay_ms(100);
-        if(usbInterruptIsReady())
+        if (TCNT0 >= 253)
+          {
+             timeout++;
+             TCNT0 = 0;
+          }
+        // We are trying to get timeout if 4000 * 15 = 60 seconds
+        // move the mouse after 1 min
+        if(usbInterruptIsReady() && timeout > 4000)
           /* called after every poll of the interrupt endpoint */
           {
              rand = (rand * 109 + 89) % 251;
@@ -157,6 +125,8 @@ int __attribute__((noreturn)) main(void)
              reportBuffer.dy = ((rand & 0xf0)>>4) - 8;
 
              usbSetInterrupt((void *)&reportBuffer, sizeof(reportBuffer));
+             timeout = 0;
+             TCNT0 = 0;
           }
      }
 }
