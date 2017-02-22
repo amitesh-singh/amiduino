049bb3007100cfa0e7f06de3e27269514b687358
Amitesh Singh <singh.amitesh@gmail.com>
2017-02-22 21:45:17 +0530

    Add comments into code.

--- stm32/README.md
+++ stm32/README.md
@@ -1,5 +1,6 @@
-### installation
+### installation & setup
 - need sudo apt-get install libusb-1.0
+- http://blog.borg.ch/?p=342
 
 ### board
 - stm32f103c8t6
--- stm32/blink/hello.c
+++ stm32/blink/hello.c
@@ -1,7 +1,7 @@
 /*
 
 
-*/
+ */
 
 #include <libopencm3/cm3/common.h>
 #include <libopencm3/stm32/f1/rcc.h>
@@ -9,26 +9,28 @@
 
 static void my_delay_1( void )
 {
-	int i = 72e6/2/4;
+   int i = 72e6/2/4;
 
-	while( i > 0 )
-	{
-		i--;
-		__asm__( "nop" );
-	}
+   while( i > 0 )
+     {
+        i--;
+        __asm__( "nop" );
+     }
 }
 
 int main( void )
 {
-	rcc_clock_setup_in_hse_8mhz_out_72mhz();
-	rcc_periph_clock_enable( RCC_GPIOC );
-	gpio_set_mode( GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
-					GPIO_CNF_OUTPUT_PUSHPULL, GPIO13 );
+   //set STM32 to 72 MHz
+   rcc_clock_setup_in_hse_8mhz_out_72mhz();
+   // Enable GPIOC clock
+   rcc_periph_clock_enable( RCC_GPIOC );
+   //Set GPIO13 (inbuild led connected) to 'output push-pull'
+   gpio_set_mode( GPIOC, GPIO_MODE_OUTPUT_2_MHZ,
+                  GPIO_CNF_OUTPUT_PUSHPULL, GPIO13 );
 
-	while( 1 )
-	{
-		gpio_toggle( GPIOC, GPIO13 );
-		my_delay_1();
-	}
+   while( 1 )
+     {
+        gpio_toggle( GPIOC, GPIO13 );
+        my_delay_1();
+     }
 }
-
