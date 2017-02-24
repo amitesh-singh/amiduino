0788137c71bdb0acdfd86cec477ea22fee06ded8
Amitesh Singh <singh.amitesh@gmail.com>
2017-02-25 00:15:47 +0530

    pwm: add readme.md

---
+++ avr/atmega328p/pwm/README.md
@@ -0,0 +1,13 @@
+## PWM modes
+
+### Fast PWM
+
+- PWM_fequency = clock_speed / [Prescaller_value * (1 + TOP_Value) ]
+
+### Phase corrected PWM
+
+- PWM_frequency = clock_speed / (2 * Prescaller_value * TOP_value )
+
+### Phase and frequency PWM
+
+- PWM_frequency = clock_speed / (2 * Prescaller_value * TOP_value )
--- avr/atmega328p/pwm/timer2/two-output/pwm.c
+++ avr/atmega328p/pwm/timer2/two-output/pwm.c
@@ -11,14 +11,14 @@ int main()
     DDRD |= (1 << PD3);
 
     OCR2A = 200; //80% duty cycle
-    OCR2B = 100; //40% duty cycle
+    OCR2B = 255; //40% duty cycle
 
     TCCR2A |= (1 << COM2A1); //enable OCR2A pin with non-inverted mode
     TCCR2A |= (1 << COM2B1); //enable OCR2B pin with non-inverted mode
     TCCR2A |= (1 << WGM20) | (1 << WGM21) | (1 << WGM22); // Fast pwm - ORC2A top?
 
-    TCCR2B |= (1 << CS21); // set prescaler to 8 and start pwm
-    
+    TCCR2B |= (1 << CS21) | (1 << CS20) | (1 << CS22); // set prescaler to 8 and start pwm
+
     while (1)
     {
 
