522035edac6372acd9a4b831884dc2bf28b6d5d0
Amitesh Singh <singh.amitesh@gmail.com>
2017-02-25 00:07:15 +0530

    pwm: add timer2 example
    both one and two output.

---
+++ avr/atmega328p/pwm/timer2/two-output/pwm.c
@@ -0,0 +1,28 @@
+// timer2 - pwm2 example - one output OCR2A pin
+// PB3 - OCR2A pin
+// PD3 - OCR2B pin
+
+#include <avr/io.h>
+
+
+int main()
+{
+    DDRB |= (1 << PB3);
+    DDRD |= (1 << PD3);
+
+    OCR2A = 200; //80% duty cycle
+    OCR2B = 100; //40% duty cycle
+
+    TCCR2A |= (1 << COM2A1); //enable OCR2A pin with non-inverted mode
+    TCCR2A |= (1 << COM2B1); //enable OCR2B pin with non-inverted mode
+    TCCR2A |= (1 << WGM20) | (1 << WGM21) | (1 << WGM22); // Fast pwm - ORC2A top?
+
+    TCCR2B |= (1 << CS21); // set prescaler to 8 and start pwm
+    
+    while (1)
+    {
+
+    }
+
+    return 0;
+}
