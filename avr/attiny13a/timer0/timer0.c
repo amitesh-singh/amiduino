5dd66aa78034aa85c7481f0eaa811b98fcbccea4
Amitesh Singh <singh.amitesh@gmail.com>
2017-06-25 20:26:02 +0900

    attiny13a: add timer0 CTC compare non-pwm example

---
+++ avr/attiny13a/timer0/timer0.c
@@ -0,0 +1,51 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include <avr/interrupt.h>
+
+#define LED_PORT PB3
+
+static void
+init_timer0()
+{
+    // Compare Output Mode, non-PWM Mode
+    cli();
+    TCCR0B |= (1 << CS02) | (1 << CS00); // f_cpu/1024
+    TCCR0A |= (1 << COM0A1); // Clear OC0A on Compare Match  
+    TIMSK0 |= 1 << TOIE0;
+
+    TCNT0 = 0;
+    OCR0A = 250;
+    sei();
+}
+
+static void
+stop_timer()
+{
+    cli();
+    TIMSK0 &= ~(1 << TOIE0);
+    sei();
+}
+
+ISR(TIM0_OVF_vect)
+{
+    PORTB ^= (1 << LED_PORT);
+}
+
+int main(void)
+{
+    DDRB |= (1 << LED_PORT);
+
+    init_timer0();
+    PORTB |= (1 << LED_PORT);
+    _delay_ms(1000);
+    PORTB &= ~(1 << LED_PORT);
+    _delay_ms(1000);
+
+    while (1)
+    {
+
+    }
+
+    return 0;
+}
