ed0f4567e2b2578368c99ef67977b9e74c406b9e
Amitesh Singh <singh.amitesh@gmail.com>
2017-09-24 13:40:38 +0900

    lollin esp12e board: Add simple  blink sketch

---
+++ esp/lollin/blink/blink.ino
@@ -0,0 +1,15 @@
+//This is LOLLIN board V3 i have, which has on board led at D4 (which is mapped to GPIO2)
+#define LOLLIN_LED D4
+
+void setup()
+{
+  pinMode(LOLLIN_LED, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(LOLLIN_LED, 1);
+  delay(1000);
+  digitalWrite(LOLLIN_LED, 0);
+  delay(1000);
+}
