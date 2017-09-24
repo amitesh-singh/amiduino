d594a765745475fd6aaddfe277dff971ce207385
Amitesh Singh <singh.amitesh@gmail.com>
2017-09-24 20:20:47 +0900

    lollino: smart humidifier - ok, it works

--- esp/lollin/smart_humidifier/smart_humidifier.ino
+++ esp/lollin/smart_humidifier/smart_humidifier.ino
@@ -1,105 +1,104 @@
+// Smart humidifier 
+//(C) Amitesh Singh
+
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
-#include <DHT.h>
+#include <SimpleDHT.h>
 
-static void _readDH11();
+#include "index.h"
+
+static void _readDHT11();
 
 static const char ssid[] = "POOJA";
 static const char password[] = "60276711";
-const char mainPage[] PROGMEM = "<html> \
-<head> \
-<meta http-equiv='refresh' content='3'> \
-<title>Smart Humidifier by Ami</title> \
-<style> \
-div.card { \
-  width: 250px; \
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19); \
-  text-align: center; \
-  border-radius: 5px; \
-  background-color: #F5F7A0 \
-} \
-div.header { \
-    background-color: #E03C3C; \
-    color: white; \
-    padding: 10px; \
-    font-size: 40px; \
-    border-radius: 5px; \
-} \
-div.container { \
-    padding: 4px; \ 
-} \
-</style> \
-</head> \
-<body> \
-<center><h1> Smart Humidifier </h1> \
-<form> \
-Set Humid Value (0-80): \
-<input type='text' name='userHumidValue' maxlength='2' size='2'> \
-<input type='submit' value='Set Humid'> <br> <br> \
-</form> \
-<div class='card'> \
-<div class='header'> \
-  <h2>@@oldhvalue@@ C</h2> \
-</div> \
-<div class='container'> \
-  <h3>Humidity</h3> \
-</div> \
-</div> \
-<div class='card'> \
-  <div class='header'> \
-    <h2>@@roomTemp@@ C</h2> \
-  </div> \
-  <div class='container'> \
-    <h3>Temperature</h3> \
-  </div> \
-</div> \
-</center> \
-</body> \
-</html>";
-
-
-static ESP8266WebServer server(80);
-
-float humidity, temp_c;
-#define DHTTYPE DHT11
-#define DHTPIN D1
-DHT dht(DHTPIN, DHTTYPE, 16);
 
 static const uint8_t led = D4;
+static const uint8_t pinDHT11 = D0;
+static const uint8_t pinFan = D1;
+static const uint8_t pinHeater = D2;
 
-static uint8_t userHumidValue = 0;
-static uint8_t humidValue =  45;
-static uint8_t roomTemp = 25;
+static uint8_t userHumidValue = 35;
+static uint8_t humidValue =  0;
+static uint8_t roomTemp = 0;
+static bool heaterOn = false;
+static bool fanOn = false;
 
-static void handleSubmit()
+
+static SimpleDHT11 dht11;
+static ESP8266WebServer server(80);
+
+static void processHumidityController()
 {
-  String newHumidVal = server.arg("userHumidValue");
-  Serial.println("Set the humid value to:");
-  Serial.print(newHumidVal);
-  if (newHumidVal.toInt() > 80 || newHumidVal.toInt() < 20)
-  {
-    humidValue = newHumidVal.toInt();
-    server.send(200, "text/html", newHumidVal);
-  }
-  else 
-    server.send(200, "text/html", "Wrong humidity value");
+   if (humidValue < userHumidValue)
+     {
+        if (!digitalRead(pinFan))
+          {
+             Serial.println("Started FAN");
+             digitalWrite(pinFan, HIGH);
+             fanOn = true;
+          }
+        if (!digitalRead(pinHeater))
+          {
+             Serial.println("Started heater:");
+             digitalWrite(pinHeater, HIGH);
+             heaterOn = true;
+          }
+     }
+   else
+     {
+        if (digitalRead(pinFan))
+          {
+             Serial.println("Stopped FAN");
+             digitalWrite(pinFan, LOW);
+             fanOn = false;
+          }
+        if (digitalRead(pinHeater))
+          {
+             Serial.println("Stopped HEATER");
+             digitalWrite(pinHeater, LOW);
+             heaterOn = false;
+          }
+     }
 }
-static void handleRoot()
+
+static void
+handleSubmit()
+{
+   String newHumidVal = server.arg("userHumidValue");
+   Serial.println("Set the humid value to:");
+   Serial.print(newHumidVal);
+   if (newHumidVal.toInt() < 80 || newHumidVal.toInt() > 10)
+     {
+        userHumidValue = newHumidVal.toInt();
+        server.send(200, "text/html", newHumidVal);
+     }
+   else
+     server.send(200, "text/html", "Wrong humidity value");
+}
+
+static void
+handleRoot()
 {
    String s = mainPage;
 
    if (server.hasArg("userHumidValue"))
-   {
-     return handleSubmit();
-   }
-   _readDH11();
-   humidValue = humidity;
-   roomTemp = temp_c;
+     {
+        return handleSubmit();
+     }
 
-   s.replace("@@oldhvalue@@", String(humidValue));
+   s.replace("@@humidity@@", String(humidValue));
    s.replace("@@roomTemp@@", String(roomTemp));
+   s.replace("@@userhval@@", String(userHumidValue));
+   if (fanOn)
+     s.replace("@@fan@@", "ON");
+   else
+     s.replace("@@fan@@", "OFF");
+   if (heaterOn)
+     s.replace("@@heater@@", "ON");
+   else
+     s.replace("@@heater@@", "OFF");
 
    server.send(200, "text/html", s);
 }
@@ -107,34 +106,39 @@ static void handleRoot()
 void
 handleNotFound()
 {
-     digitalWrite(led, 1);
-     String message = "File Not Found\n\n";
-     message += "URI: ";
-     message += server.uri();
-     message += "\nMethod: ";
-     message += (server.method() == HTTP_GET)?"GET":"POST";
-     message += "\nArguments: ";
-     message += server.args();
-     message += "\n";
-     for (uint8_t i=0; i<server.args(); i++){
-          message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-     }
-     server.send(404, "text/plain", message);
-     digitalWrite(led, 0);
+   digitalWrite(led, 1);
+   String message = "File Not Found\n\n";
+   message += "URI: ";
+   message += server.uri();
+   message += "\nMethod: ";
+   message += (server.method() == HTTP_GET)?"GET":"POST";
+   message += "\nArguments: ";
+   message += server.args();
+   message += "\n";
+   for (uint8_t i=0; i<server.args(); i++){
+        message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+   }
+   server.send(404, "text/plain", message);
+   digitalWrite(led, 0);
 }
 
-static void _timer1_cb()
+static volatile bool _forceReadDHT = false;
+
+static void
+_timer1_cb()
 {
-  Serial.println("Timer callback: going to read sensor");
-  _readDH11();
+   //Reading sensor value in timer1 - not able to read data.. don;t know why
+   //_readDH11();
+   _forceReadDHT = true;
 }
 
 void setup(void)
 {
    pinMode(led, OUTPUT);
+   pinMode(pinFan, OUTPUT);
+   pinMode(pinHeater, OUTPUT);
+
    digitalWrite(led, 0);
-   //initialize DHT sensor
-   dht.begin();
 
    Serial.begin(115200);
    WiFi.begin(ssid, password);
@@ -158,39 +162,52 @@ void setup(void)
      }
 
    server.on("/", handleRoot);
-   server.on("/inline", [](){
-             server.send(200, "text/plain", "this works as well");
-             });
 
    server.onNotFound(handleNotFound);
 
    server.begin();
    Serial.println("HTTP server started");
 
-   _readDH11();
+   _readDHT11();
+
    //start timer1
    timer1_disable();
    timer1_attachInterrupt(_timer1_cb);
    timer1_isr_init();
 
    timer1_enable(TIM_DIV265, TIM_EDGE, TIM_LOOP);
-   timer1_write(ESP.getCycleCount() + 80e6);
+   timer1_write(ESP.getCycleCount() + 160e6);
 }
 
 void loop(void)
 {
    server.handleClient();
+   if (_forceReadDHT)
+     {
+        _readDHT11();
+        processHumidityController();
+        _forceReadDHT = false;
+     }
 }
 
 static void
-_readDH11()
+_readDHT11()
 {
-  digitalWrite(led, !digitalRead(led));
-  
-  humidity = dht.readHumidity();
-  temp_c = dht.readTemperature();
-  Serial.print("Humidity: ");
-  Serial.print(String(humidity));
-  Serial.print("  Temperature: ");
-  Serial.println(temp_c);
+   digitalWrite(led, !digitalRead(led));
+
+   humidValue = 0;
+   roomTemp = 0;
+   int err = SimpleDHTErrSuccess;
+   if ((err = dht11.read(pinDHT11, &roomTemp, &humidValue, NULL))
+       != SimpleDHTErrSuccess)
+     {
+        Serial.print("Read DHT11 failed, err="); Serial.println(err);
+        delay(1000);
+        return;
+     }
+
+   Serial.print("Humidity: ");
+   Serial.print(String(humidValue));
+   Serial.print("  Temperature: ");
+   Serial.println(String(roomTemp));
 }
