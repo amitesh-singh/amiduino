51bd71f84809a10fd440313da7cc9288634a3094
Amitesh Singh <singh.amitesh@gmail.com>
2017-10-28 22:36:56 +0900

    esp12e: timer0 - make example better

--- esp/esp12e/timers/timer0/timer0.ino
+++ esp/esp12e/timers/timer0/timer0.ino
@@ -1,14 +1,17 @@
 
 #include <Arduino.h>
-// This example blinks the LED (GPIO_13) in every 1 second
+
+#define led 2
+
+// This example blinks the LED (led) in every 1 second
 static const unsigned long CPU_MHZ = 80000000; //80Mhz -> 80*10^6 = 1 second
-volatile uint8_t toggle;
+//volatile uint8_t toggle;
 
 void inline timer0_ISR(void)
 {
-   toggle = (toggle == 1) ? 0: 1;
-   digitalWrite(13, toggle);
-
+   //toggle = (toggle == 1) ? 0: 1;
+   //digitalWrite(led, toggle);
+   digitalWrite(led, !digitalRead(led)); 
    // if you want timer0 to be called just once, don't call timer0_write the below stuff
    // Set-up the next interrupt cycle
    timer0_write(ESP.getCycleCount() + CPU_MHZ);
@@ -16,7 +19,7 @@ void inline timer0_ISR(void)
 
 void setup()
 {
-   pinMode(13, OUTPUT);
+   pinMode(led, OUTPUT);
    noInterrupts();
    timer0_isr_init();
    timer0_attachInterrupt(timer0_ISR);
