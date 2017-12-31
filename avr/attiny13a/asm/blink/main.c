6ccd248a08133a5fc51e186bb4c5477d13727513
Amitesh Singh <singh.amitesh@gmail.com>
2017-12-31 11:12:28 +0900

    fix code to work with assembly and C

--- avr/attiny13a/asm/blink/main.c
+++ avr/attiny13a/asm/blink/main.c
@@ -1,13 +1,21 @@
 #include <avr/io.h>
+#include <util/delay.h>
 
 extern void led_init();
-extern void loop();
+extern void set_pb0_high();
+extern void set_pb0_low();
 
 int main()
 {
   led_init();
-    
-  loop();
+  
+  while (1)
+  {
+    set_pb0_high();
+    _delay_ms(1000);
+    set_pb0_low();
+    _delay_ms(1000);
+  }
 
   return 0;
 }
