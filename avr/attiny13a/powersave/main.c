2cfd48ae4b46ceda773647ac7bdba454fa179596
Amitesh Singh <singh.amitesh@gmail.com>
2018-01-17 21:46:44 +0900

    indent correction.

--- avr/attiny13a/powersave/main.c
+++ avr/attiny13a/powersave/main.c
@@ -12,11 +12,11 @@ ISR(WDT_vect)
 static void
 save_more_power()
 {
-  //without this below code, the current consumption is around 0.25mA
-  //turn off ADC
-  ADCSRA &= ~(1 << ADEN); // this gives 0.005mA at 3.3v
-  ACSR = (1<<ACD); //Disable the analog comparator
-  DIDR0 = 0x3F; //Disable digital input buffers on all ADC0-ADC5 pins.
+   //without this below code, the current consumption is around 0.25mA
+   //turn off ADC
+   ADCSRA &= ~(1 << ADEN); // this gives 0.005mA at 3.3v
+   ACSR = (1<<ACD); //Disable the analog comparator
+   DIDR0 = 0x3F; //Disable digital input buffers on all ADC0-ADC5 pins.
 }
 
 int main(void)
@@ -31,8 +31,8 @@ int main(void)
 
    while (1)
      {
-       	set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-       	sleep_mode(); // <-- sleeps here
+        set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+        sleep_mode(); // <-- sleeps here
         //wokes up after 8s and print this message.
         uart_puts("wokeup\r\n");
      }
