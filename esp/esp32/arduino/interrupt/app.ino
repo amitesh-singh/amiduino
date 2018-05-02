bebf5eaf96e432118e4c70dc1e748233d9cf2ae9
Amitesh Singh <singh.amitesh@gmail.com>
2018-05-02 20:48:58 +0530

    esp32: Add more comments into the interrupt example code.

--- esp/esp32/arduino/interrupt/app.ino
+++ esp/esp32/arduino/interrupt/app.ino
@@ -3,10 +3,16 @@ const int interruptPin = 25; //GPIO25
 volatile int interruptCounter = 0;
 int numberOfInterrupts = 0;
 
+//IRAM_ATTR tells the complier, that this code Must always be in the 
+// ESP32's IRAM, the limited 128k IRAM.  use it sparingly.
 void IRAM_ATTR handleInterrupt()
 {
     interruptCounter++;
 }
+
+// Every function called from the ISR also needs to be 
+// declared as IRAM_ATTR yourFunction() {}.
+// Also don't call analogRead() in ISR either.
 
 void setup()
 {
