746484a629e3ef0996639878774112718ac1d171
Amitesh Singh <singh.amitesh@gmail.com>
2018-10-08 20:07:45 +0530

    modify changemac code

--- esp/esp12e/changemac/changemac.ino
+++ esp/esp12e/changemac/changemac.ino
@@ -3,7 +3,8 @@
 void changeMac()
 {
    static uint8_t mac[] = {0x77, 0x01, 0x02, 0x03, 0x04, 0x05};
-   wifi_set_macaddr(STATION_IF, &mac[0]);
+   //wifi_set_macaddr(STATION_IF, &mac[0]);
+   wifi_set_macaddr(SOFTAP_IF, &mac[0]);
 }
 
 void setup()
@@ -15,12 +16,16 @@ void setup()
         Serial.print(".");
         delay(100);
     }
+    WiFi.mode(WIFI_STA);
+  
     Serial.println("");
     Serial.print("mac: "); Serial.println(WiFi.macAddress());
-
+    Serial.print("soft mac: "); Serial.println(WiFi.softAPmacAddress());
+    //this does not work.
     changeMac();
 
     Serial.print("new mac: "); Serial.println(WiFi.macAddress());
+    Serial.print("soft mac: "); Serial.println(WiFi.softAPmacAddress());
 }
 
 void loop()
