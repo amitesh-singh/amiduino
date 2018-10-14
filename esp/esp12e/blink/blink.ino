9cc089349e1eda527925f824543835bf78a6a1c5
Amitesh Singh <singh.amitesh@gmail.com>
2018-10-14 21:20:05 +0530

    esp: blink: modify blink code

--- esp/esp12e/blink/blink.ino
+++ esp/esp12e/blink/blink.ino
@@ -1,19 +1,22 @@
 //This is LOLLIN board V3 i have, which has on board led at D4 (which is mapped to GPIO2)
-#define LOLLIN_LED D4
+#define LOLLIN_LED BUILTIN_LED
 //In my esp12e board on Perfboard has LED on Digital port 2 
 #define MYESPBOARD_LED 2
 
 void setup()
 {
-  pinMode(MYESPBOARD_LED, OUTPUT);
-  Serial.begin(115200);
+  pinMode(LOLLIN_LED, OUTPUT);
+  Serial.begin(9600);
+
+  pinMode(5, OUTPUT);
+  digitalWrite(5, 1);
 }
 
 void loop()
 {
-  digitalWrite(MYESPBOARD_LED, 1);
+  digitalWrite(LOLLIN_LED, 1);
   delay(1000);
   Serial.println("Blinks");
-  digitalWrite(MYESPBOARD_LED, 0);
+  digitalWrite(LOLLIN_LED, 0);
   delay(1000);
 }
