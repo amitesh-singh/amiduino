fa9e68aeaafda03de9a58ab59860eaaa47ac4418
Amitesh Singh <singh.amitesh@gmail.com>
2018-11-01 23:47:14 +0530

    esp12e: Add OTA code

---
+++ esp/esp12e/ota/ota.ino
@@ -0,0 +1,98 @@
+// this would works for esp12 and other versions which has flash = 4MB
+
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+#include <Ticker.h>
+
+const char* ssid = "AndroidHotspot4095";
+const char* password = "";
+Ticker blinker;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED)
+    {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("myesp8266");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword("admin");
+
+  // Password can be set with it's md5 value as well
+  // MD5(admin) = 21232f297a57a5a743894a0e4a801fc3
+  // ArduinoOTA.setPasswordHash("21232f297a57a5a743894a0e4a801fc3");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+    {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  pinMode(BUILTIN_LED, OUTPUT);
+  blinker.attach(0.5, _blink_cb);
+}
+
+void _blink_cb()
+{
+  digitalWrite(BUILTIN_LED, !digitalRead(BUILTIN_LED));
+}
+
+void loop()
+{
+   // we can have a btn to enable this OTA mode, in case we have to wait in loop or we
+   // are doing heavy tasks in loop()
+  ArduinoOTA.handle();
+  //it won't work, since handle() needs to be called all the time.
+  /*
+  delay(200);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(200);
+  digitalWrite(BUILTIN_LED, HIGH);
+  */
+}
