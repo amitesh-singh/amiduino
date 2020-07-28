97e89b9f407b20201cb01d46ddad5c85bb5e896f
Amitesh Singh <singh.amitesh@gmail.com>
2020-07-28 20:30:01 +0530

    kivy: start kivy learning journey

---
+++ gui/kivy/label/README.md
@@ -0,0 +1,4 @@
+sudo chmod 777 /dev/input/event8
+
+to get rid of warning msgs
+
---
+++ gui/kivy/label/simple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import kivy
+
+kivy.require('1.11.1')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello world')
+
+if __name__ == '__main__':
+    MyApp().run()
