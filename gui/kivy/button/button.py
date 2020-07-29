24350285990a2189c6e9fe53dc6499666bea78d3
Amitesh Singh <singh.amitesh@gmail.com>
2020-07-29 20:23:00 +0530

    kivy: btn - implement btn with pressed event

--- gui/kivy/button/button.py
+++ gui/kivy/button/button.py
@@ -3,9 +3,18 @@ import kivy
 from kivy.app import App
 from kivy.uix.button import Button
 
+
 class MyApp(App):
     def build(self):
-        return Button(text='hello world')
+        self.btn = Button(text='hello world')
+        self.btn.bind(on_press=self.pressed)
+
+        return self.btn
+
+
+    def pressed(self, instance):
+        print('pressed')
+
 
 if __name__ == '__main__':
     MyApp().run()
