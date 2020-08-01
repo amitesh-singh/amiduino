8385dcbc714a19ca38f45d32aa9bd5c7653b7291
Amitesh Singh <singh.amitesh@gmail.com>
2020-08-01 13:23:32 +0530

    kivy: add one more kivy example.

---
+++ gui/kivy/kvadvanced/main.py
@@ -0,0 +1,21 @@
+from sys import platform
+import os
+if platform == 'win32':
+    os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
+
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MyGrid(Widget):
+    pass
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+
+if __name__ == '__main__':
+    MyApp().run()
