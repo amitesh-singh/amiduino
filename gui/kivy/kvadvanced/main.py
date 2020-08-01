1af2cd5f88f94a5a515ce8b43c312f2d80944b33
Amitesh Singh <singh.amitesh@gmail.com>
2020-08-01 14:04:26 +0530

    make it better

--- gui/kivy/kvadvanced/main.py
+++ gui/kivy/kvadvanced/main.py
@@ -6,15 +6,20 @@ if platform == 'win32':
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
 class MyGrid(Widget):
-    pass
+    name = ObjectProperty(None)
+    email = ObjectProperty(None)
+
+    def btnclick(self):
+        print("Name: ", self.name.text, " Email: ", self.email.text)
+
 
 class MyApp(App):
     def build(self):
         return MyGrid()
-
 
 
 if __name__ == '__main__':
