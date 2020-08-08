001cb8d8cf285eb5aa939314989ee176951a2f8a
Amitesh Singh <singh.amitesh@gmail.com>
2020-08-08 16:02:24 +0530

    Add clock

---
+++ gui/kivy/clock/main.py
@@ -0,0 +1,27 @@
+import kivy
+from kivy.uix.button import Button
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+
+
+class MyBoxLayout(BoxLayout):
+    # dt means delta-time
+    def callback(self, dt):
+        print("my callback is called")
+
+    # call my_callback in 5 seconds
+    def clickme(self):
+        Clock.schedule_once(self.callback, 2) 
+
+    def clickme2(self):
+        Clock.schedule_interval(self.callback, 5)
+
+
+class MyApp(App):
+    def build(self):
+        return MyBoxLayout()
+    
+
+MyApp().run()
+
