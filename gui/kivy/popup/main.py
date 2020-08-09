f4afcf60390f95a3d75db602e53803539ad4110f
Amitesh Singh <singh.amitesh@gmail.com>
2020-08-09 13:46:19 +0530

    implement timeout

--- gui/kivy/popup/main.py
+++ gui/kivy/popup/main.py
@@ -5,16 +5,24 @@ from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from mypopup import MyPopup
+from kivy.clock import Clock
 
+global popup
 
 class MyButton(BoxLayout):
+    def dismiss_popup(self, dt):
+        global popup
+        popup.dismiss()
+
     def createpopup(self):
+        global popup
         popup = Popup(title='Error',
                       title_color=[1, 0, 0, 1],
                       content=Label(text='Hello world'),
                       size_hint=(None, None), size=(100, 100),
                       )
         popup.open()
+        Clock.schedule_once(self.dismiss_popup, 5)
 
     def createrightalignpopup(self):
         popup = Popup(title='Error',
@@ -23,7 +31,7 @@ class MyButton(BoxLayout):
                       size_hint=(0.5, 0.5), size=(200, 200),
                       pos_hint={'right': 0.9, 'top': 1})
         popup.open()
-    
+
     def mypopup(self):
         MyPopup().show(title='error', msg='Unable to connct')
 
--- gui/kivy/popup/mypopup.py
+++ gui/kivy/popup/mypopup.py
@@ -1,13 +1,21 @@
 import kivy
 from kivy.uix.popup import Popup
 from kivy.uix.label import Label
+from kivy.clock import Clock
 
 
 class MyPopup:
+    def dismiss_popup(self, dt):
+        self.popup.dismiss()
+        print("dismisspopup is called.")
+
     def show(self, title, msg):
-        popup = Popup(title=title,
+        self.popup = Popup(title=title,
                       title_color=[1, 0, 0, 1],
                       content=Label(text=msg),
                       size_hint=(0.5, 0.5), size=(200, 200),
                       pos_hint={'right': 0.9, 'top': 1})
-        popup.open()
+        
+        Clock.schedule_once(self.dismiss_popup, 2)
+
+        self.popup.open()
