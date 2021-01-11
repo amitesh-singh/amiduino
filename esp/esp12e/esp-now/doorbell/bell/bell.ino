cee669bed88c607fa9ac912e4120fe9b60d92600
Amitesh Singh <singh.amitesh@gmail.com>
2021-01-12 00:02:28 +0530

    make the tone better.

--- esp/esp12e/esp-now/doorbell/bell/bell.ino
+++ esp/esp12e/esp-now/doorbell/bell/bell.ino
@@ -142,7 +142,7 @@ void loop()
        //mytone.play_ringer(D2);
        //mytone.play_close_encounter(D2);
        mytone.play_tone_up(D2);
-       delay(200);
+       delay(500);
 
        mytone.play_tone_down(D2);
        delay(1000);
