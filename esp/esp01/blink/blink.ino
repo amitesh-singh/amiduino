ee98655f08887c1a727afd941bf8c56d408f9722
Amitesh Singh <singh.amitesh@gmail.com>
2022-01-15 16:35:44 +0530

    led builtin it at led pin 1

--- esp/esp01/blink/blink.ino
+++ esp/esp01/blink/blink.ino
@@ -10,18 +10,19 @@
  */
 
 // In pins_arduino.h, LED_BUILTIN is defined as 1 which is GPIO_1 actually (TXD)
+uint8_t led = 1;
 void setup()
 {
-   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+   pinMode(led, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
 }
 
 // the loop function runs over and over again forever
 void loop()
 {
-   digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on (Note that LOW is the voltage level
+   digitalWrite(led, LOW);   // Turn the LED on (Note that LOW is the voltage level
    // but actually the LED is on; this is because 
    // it is acive low on the ESP-01)
    delay(1000);                      // Wait for a second
-   digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off by making the voltage HIGH
+   digitalWrite(led, HIGH);  // Turn the LED off by making the voltage HIGH
    delay(2000);                      // Wait for two seconds (to demonstrate the active low LED)
 }
