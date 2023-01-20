3ec11b47199173363e2d4f10280bf5f9f4fe90ba
Amitesh Singh <singh.amitesh@gmail.com>
2023-01-20 12:41:16 +0530

    esp32 cam: Add face detection example

---
+++ esp/esp32-cam/face_detection/face_detection.ino
@@ -0,0 +1,150 @@
+#include "esp_camera.h"
+
+//Headers for detecting human face
+#include "human_face_detect_msr01.hpp" //need it for both one and two stage
+#include "human_face_detect_mnp01.hpp" // need it only for two stage detection
+
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <AsyncElegantOTA.h>
+
+static constexpr const auto ssid {"Aviaarav-2.4G"};
+static constexpr const auto passwd { "poojasingh" };
+
+AsyncWebServer server(80);
+
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+bool initCamera()
+{
+  camera_config_t config;
+ 
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_QVGA;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+
+  esp_err_t result = esp_camera_init(&config);
+ 
+  if (result != ESP_OK) {
+    return false;
+  }
+ 
+  return true;
+}
+
+#define TWO_STAGE 1 /*1: detect by two stage which is more accurate but slower (with keypoints) */
+                    /* 0: detect by one stage which is less accurate but faster (without keypoints) */
+
+
+void detect_human_face()
+{
+    camera_fb_t *fb = NULL;
+    fb = esp_camera_fb_get();
+
+    if (!fb)
+    {
+        Serial.println("unable to get FB from camera");
+        return;
+    }
+  //esp-dl is used for detecting the human face and recognition.
+    #if TWO_STAGE
+    HumanFaceDetectMSR01 s1(0.1F, 0.5F, 10, 0.2F);
+    HumanFaceDetectMNP01 s2(0.5F, 0.3F, 5);
+#else // ONE_STAGE
+    HumanFaceDetectMSR01 s1(0.3F, 0.5F, 10, 0.2F);
+#endif
+#if TWO_STAGE
+    //std::list<dl::detect::result_t> &candidates = s1.infer((uint8_t *)IMAGE_ELEMENT, {IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL});
+    //std::list<dl::detect::result_t> &results = s2.infer((uint8_t *)IMAGE_ELEMENT, {IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL}, candidates);
+    std::list<dl::detect::result_t> &candidates = s1.infer((uint16_t *)fb->buf, {(int)fb->height, (int)fb->width, 3});
+    std::list<dl::detect::result_t> &results = s2.infer((uint16_t *)fb->buf, {(int)fb->height, (int)fb->width, 3}, candidates);
+#else // ONE_STAGE
+    //std::list<dl::detect::result_t> &results = s1.infer((uint8_t *)IMAGE_ELEMENT, {IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNEL});
+    std::list<dl::detect::result_t> &results = s1.infer((uint16_t *)fb->buf, {(int)fb->height, (int)fb->width, 3});
+#endif
+    esp_camera_fb_return(fb);
+    Serial.printf("number of faces: %d", results.size());
+    uint8_t i = 0;
+    for (std::list<dl::detect::result_t>::iterator prediction = results.begin(); prediction != results.end(); prediction++, i++)
+    {
+        Serial.printf("[%d] score: %f, box: [%d, %d, %d, %d]\n", i, prediction->score, prediction->box[0], prediction->box[1], prediction->box[2], prediction->box[3]);
+#if TWO_STAGE
+        Serial.printf("    left eye: (%3d, %3d), ", prediction->keypoint[0], prediction->keypoint[1]);
+        Serial.printf("right eye: (%3d, %3d)\n", prediction->keypoint[6], prediction->keypoint[7]);
+        Serial.printf("    nose: (%3d, %3d)\n", prediction->keypoint[4], prediction->keypoint[5]);
+        Serial.printf("    mouth left: (%3d, %3d), ", prediction->keypoint[2], prediction->keypoint[3]);
+        Serial.printf("mouth right: (%3d, %3d)\n\n", prediction->keypoint[8], prediction->keypoint[9]);
+#endif
+    }
+}
+
+void setup()
+{
+    Serial.begin(115200);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, passwd);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    if (!initCamera())
+    {
+        Serial.printf("Failed to initialize Camera..");
+    }
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(200, "text/plain", "Hi! I am ESP32 camera to detect human face");
+    });
+
+    AsyncElegantOTA.begin(&server);
+    server.begin();  
+}
+
+void loop()
+{
+    detect_human_face();
+}
