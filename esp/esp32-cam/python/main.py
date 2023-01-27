8ff69448075bfddaa2398dd06d6add91d85596fd
Amitesh Singh <singh.amitesh@gmail.com>
2023-01-27 13:24:00 +0530

    made it work

--- esp/esp32-cam/python/main.py
+++ esp/esp32-cam/python/main.py
@@ -2,6 +2,9 @@ import cv2
 import urllib.request
 import numpy as np
 
+faceCascade= cv2.CascadeClassifier("./res/haarcascade_frontalface_default.xml")
+eyeCascade=cv2.CascadeClassifier('./res/haarcascade_eye.xml')
+
 url ="http://192.168.1.248:81/stream"
 cap = cv2.VideoCapture(url)
 
@@ -10,6 +13,15 @@ while True:
     if not success:
         print("unable to get stream")
         break
+    
+    faces = faceCascade.detectMultiScale(img,1.1,4)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+        roi_gray = img[y: y +h, x: x+w]
+        roi_color = img[y: y+h, x: x+ w]
+        eyes = eyeCascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 1)
 
     cv2.imshow("stream", img)
 
