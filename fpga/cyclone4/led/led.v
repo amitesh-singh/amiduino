14f80950fd1a972c2b6d6d62d7cd9bd2b628f702
Amitesh Singh <singh.amitesh@gmail.com>
2024-08-31 21:40:17 +0530

    Add first fpga program with cyclone 4 board

---
+++ fpga/cyclone4/led/led.v
@@ -0,0 +1,9 @@
+module led(output led1, output led2, output led3, output led4);
+//input s1; // Location: PIN_88, I/O Standard: 3.3-V LVTTL, Current Strength: 8mA
+//input s2; // Location: PIN_89, I/O Standard: 3.3-V LVTTL, Current Strength: 8mA
+	//output led1; // Location: PIN_87, I/O Standard: 3.3-V LVTTL, Current Strength: 8mA
+	assign led1 = 0; //this will switch on the led1
+	assign led2 = 0; //this will switch on the led2
+	assign led3 = 0;
+	assign led4 = 0;
+endmodule
