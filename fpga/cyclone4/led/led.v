dde0003a363322d5464998f1565fecc540d09d45
Amitesh Singh <singh.amitesh@gmail.com>
2024-08-31 21:46:20 +0530

    more doc

--- fpga/cyclone4/led/led.v
+++ fpga/cyclone4/led/led.v
@@ -4,6 +4,6 @@ module led(output led1, output led2, output led3, output led4);
 	//output led1; // Location: PIN_87, I/O Standard: 3.3-V LVTTL, Current Strength: 8mA
 	assign led1 = 0; //this will switch on the led1
 	assign led2 = 0; //this will switch on the led2
-	assign led3 = 0;
-	assign led4 = 0;
+	assign led3 = 0; //this will switch on the led3
+	assign led4 = 0; //this will switch on the led4
 endmodule
