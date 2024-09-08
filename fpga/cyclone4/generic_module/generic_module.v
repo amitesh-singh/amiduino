89b21abf703d80c19c36e656aa9f704d6b307c1c
Amitesh Singh <singh.amitesh@gmail.com>
2024-09-08 21:08:39 +0530

    fpga: make it truely generic; very nice

--- fpga/cyclone4/generic_module/generic_module.v
+++ fpga/cyclone4/generic_module/generic_module.v
@@ -1,3 +1,8 @@
-module generic_module(input clk, output led1);
-blink_led #(0.2) led(.clock(clk), .led(led1));
+module generic_module(input clk, 
+                      output led1, 
+                      output led2,
+                      output led3);
+blink_led #(0.2) myboard_led1(.clock(clk), .led(led1));
+blink_led #(0.5) myboard_led2(clk, led2);
+blink_led #(1) myboard_led3(clk, led3);
 endmodule
