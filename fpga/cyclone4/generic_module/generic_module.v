56d168db004368c0ca5e25ec10b3ce33d62e1cb3
Amitesh Singh <singh.amitesh@gmail.com>
2024-09-08 22:18:04 +0530

    fpga: improvement over generic module

--- fpga/cyclone4/generic_module/generic_module.v
+++ fpga/cyclone4/generic_module/generic_module.v
@@ -1,8 +1,12 @@
 module generic_module(input clk, 
                       output led1, 
                       output led2,
-                      output led3);
+                      output led3,
+                      output led4);
+
 blink_led #(0.2) myboard_led1(.clock(clk), .led(led1));
 blink_led #(0.5) myboard_led2(clk, led2);
 blink_led #(1) myboard_led3(clk, led3);
+blink_led #(2) myboard_led4(clk, led4);
+
 endmodule
